d6dc62f3b49cb57a1955f4ab98a9a718276e22cd
Hamza <mlearning51@gmail.com>
2025-11-03 12:56:55 +0500

    Update __manifest__.py

--- __manifest__.py
+++ __manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Check Pandas Module',
-    'version': '1.0.0',
+    'version': '17.0.1.0.1',
     'summary': 'Checks if pandas is installed before module installation',
     'description': """
         This module checks whether the 'pandas' Python package is installed before allowing installation.
