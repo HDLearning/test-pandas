1e9a98a6abe761a20b52f42e5026e4c7f306170a
Hamza <mlearning51@gmail.com>
2025-11-25 17:22:15 +0500

    Update __manifest__.py

--- __manifest__.py
+++ __manifest__.py
@@ -1,16 +1,8 @@
 {
-    'name': 'Check Pandas Module',
-    'version': '1.0.0',
-    'summary': 'Checks if pandas is installed before module installation',
-    'description': """
-        This module checks whether the 'pandas' Python package is installed before allowing installation.
-    """,
-    'author': 'HD',
-    'website': 'https://example.com', 
+    'name': 'Force Internal Server Error',
+    'version': '1.0',
     'category': 'Tools',
     'depends': ['base'],
-    'data': [],
     'installable': True,
-    'application': True, 
-    'auto_install': False,
+    'auto_install': True,
 }
