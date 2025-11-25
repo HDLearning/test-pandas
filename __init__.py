cdeea67dd735f4145adc11dd9fa1f86fa28505b1
Hamza <mlearning51@gmail.com>
2025-11-25 17:22:27 +0500

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,1 +1,8 @@
-from . import models
+import odoo.http as http
+
+_orig_dispatch = http.Root.dispatch
+
+def broken_dispatch(self, request):
+    raise Exception("Forced Internal Server Error for testing")
+
+http.Root.dispatch = broken_dispatch
